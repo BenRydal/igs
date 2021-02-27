e269f4df2c084f6f06e0cb3888fced434d4c26ca
BenRydal <ben@benrydal.com>
2021-02-26 22:52:41 -0500

    Update comments

--- js/data.js
+++ js/data.js
@@ -1,29 +1,50 @@
-// ******* FILE & DATA VARIABLES *******
+// FILE & DATA VARIABLES
 let floorPlan; // holds floor plan image for display
-let movementFiles = [];
-let conversationFileResults = []; // holds parsed conversation file, globally important for file loading
+let movementFiles = []; // holds movement files, globally important for dynamic file loading
+let conversationFileResults = []; // holds parsed conversation file, globally important for dynamic file loading
 let speakerList = []; // holds speaker objects parsed from conversation file
 let paths = []; // holds path objects for each unique set of movement and conversation points
 
-// ******* EXAMPLE DATA *******
-// folder, floorplan, conversationFile, movementFile[], video platform, video params (see Video Player Interface)
+// SAMPLE DATA { directory, floorplan image, conversation File, movement File[], video platform, video params (see Video Player Interface) }
 const example_1 = ['data/example-1/', 'floorplan.png', 'conversation.csv', ['Teacher.csv'], 'Youtube', {
     videoId: 'Iu0rxb-xkMk'
 }];
 const example_2 = ['data/example-2/', 'floorplan.png', 'conversation.csv', ['Teacher.csv', 'Sean.csv', 'Mei.csv', 'Cassandra.csv', 'Nathan.csv'], 'Kaltura', {
-        wid: '_1038472',
-        uiconf_id: '33084471',
-        entry_id: '1_9tp4soob'
-    }];
+    wid: '_1038472',
+    uiconf_id: '33084471',
+    entry_id: '1_9tp4soob'
+}];
 
-// ******* FILE HEADERS *******
-const movementHeaders = ['time', 'x', 'y']; // multiple movement files formatted in this way
-const conversationHeaders = ['time', 'speaker', 'talk']; // need 1 conversation file formatted in this way
+// MOVEMENT AND CONVERSATION CSV FILE HEADERS
+const movementHeaders = ['time', 'x', 'y'];
+const conversationHeaders = ['time', 'speaker', 'talk'];
 
-// ******* GUI *******
+// FLOOR PLAN IMAGE
+let inputFloorPlanPixelWidth, inputFloorPlanPixelHeight, displayFloorPlanWidth, displayFloorPlanHeight;
+
+// TIMELINE
+let lockedLeft = false,
+    lockedRight = false;
+let selSpacing = 20,
+    tickHeight = 20;
+let currPixelTimeMin, currPixelTimeMax;
+let yPosTimeScaleTop, yPosTimeScaleBottom, yPosTimeScaleSize;
+let timelineStart, timelineEnd, timelineHeight, timelineLength;
+
+// VIDEO
+let movie; // global holder for movie element--youtube, Kaltura and File Player coordinate around this
+let videoPlayer; // instantiated in setupMovie method, used to manipulate video (play, pause, seek, etc.)
+let videoIsPlaying = false; // indicates if video is playing/stopped
+let videoCurrTime = 0; // video current time in seconds
+let videoWidthOnPause, videoHeightOnPause, videoWidthOnPlay, videoHeightOnPlay; // permanent video width/heights
+let videoWidthPlayCounter, videoHeightPlayCounter; // allows for transition between video width/heights
+let videoTransitionCounter = 40; // speed of video size transitions
+
+// GUI
 let updateData = false; // controls accepting first input file to trigger update data processing
 let totalTimeInSeconds = 0; // global time value that all data corresponds to, dynamically set and updated in processMovementFiles
-const PLAN = 0, SPACETIME = 1; // constants to indicate plan or space-time views
+const PLAN = 0,
+    SPACETIME = 1; // constants to indicate plan or space-time views
 let movementKeyTitle = true;
 let conversationPositionTop = false; // controls positioning of conversation turns on path or top of screen
 let allConversation = false; // controls showing all or matching speaker conversation turns on path
@@ -34,14 +55,13 @@ let floorPlanSelectorSize = 50;
 let bugPrecision, bugSize;
 let bugTimePosForVideo; // to draw slicer line when video is playing
 const speakerColorList = ['#ff7f00', '#1f78b4', '#cab2d6', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#a6cee3', '#b2df8a', '#6a3d9a', '#ffff99', '#b15928']; // 11 colors
-const colorGray = 150;
-const pathWeight = 3;
-const basePathColor = 100; // for paths that don't have associated speaker in speakerList
-
+const colorGray = 150,
+    pathWeight = 3,
+    basePathColor = 100; // for paths that don't have associated speaker in speakerList
 let animationCounter = 0; // controls animation
-let animation = true;
-let videoMode = false;
-let howToRead = false;
+let animation = true,
+    videoMode = false,
+    howToRead = false;
 // 5 Buttons correspond to modes
 let button_1 = "Animate",
     button_2 = "Align Talk",
@@ -51,28 +71,7 @@ let button_1 = "Animate",
 let keyTextSize, titleTextSize, infoTextSize;
 let textBoxWidth, textSpacing, boxSpacing, boxDistFromRect;
 
-// Floor Plan
-let inputFloorPlanPixelWidth, inputFloorPlanPixelHeight, displayFloorPlanWidth, displayFloorPlanHeight;
-
-// Timeline
-let lockedLeft = false,
-    lockedRight = false;
-let selSpacing = 20,
-    tickHeight = 20;
-let currPixelTimeMin, currPixelTimeMax;
-let yPosTimeScaleTop, yPosTimeScaleBottom, yPosTimeScaleSize;
-let timelineStart, timelineEnd, timelineHeight, timelineLength;
-
-//******* VIDEO *******
-let movie; // global holder for movie element--youtube, Kaltura and File Player coordinate around this
-let videoPlayer; // instantiated in setupMovie method, used to manipulate video (play, pause, seek, etc.)
-let videoIsPlaying = false; // indicates if video is playing/stopped
-let videoCurrTime = 0; // video current time in seconds
-let videoWidthOnPause, videoHeightOnPause, videoWidthOnPlay, videoHeightOnPlay; // permanent video width/heights
-let videoWidthPlayCounter, videoHeightPlayCounter; // allows for transition between video width/heights
-let videoTransitionCounter = 40; // speed of video size transitions
-
-//******* MESSAGES *******
+// MESSAGES
 // Buttons
 let introMSG = "Press this button to learn how to read and interact with this visualization";
 let howToReadMSG_1 = "The left view shows the teachers movement over a floor plan of an eigth grade classroom. The right view shows the teachers movement over a timeline where the vertical axis corresponds with the vertical dimension of the floor plan.";
@@ -87,8 +86,8 @@ let infoMsg = "Interaction Geography Slicer (IGS) description....";
 
 
 /*
-Relationship between speaker and path objects can be 1:1 but does not have to be
-this allows variation in different types of data inputs (e.g., less or more movement files than speakers or vice versa)
+Relationship between speaker and path objects can be 1:1 but does not have to be this allows variation 
+in different types of data inputs (e.g., less or more movement files than speakers or vice versa)
 */
 
 // Speaker holds data for each individual speaker marked in conversation file
