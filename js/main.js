2744e8209a858f9c0f42c95d8d12dddb1d648aef
BenRydal <ben@benrydal.com>
2021-03-31 14:32:45 -0400

    Update animation rate to keep constate speed

--- js/main.js
+++ js/main.js
@@ -164,7 +164,12 @@ function draw() {
 }
 
 function setUpAnimation() {
-    if (animationCounter < map(currPixelTimeMax, timelineStart, timelineEnd, 0, totalTimeInSeconds)) animationCounter++; // updates animation
+    let animationIncrementRateDivisor = 1000; // this seems to work best
+    // Get amount of time in seconds currently displayed
+    let curTimeIntervalInSeconds = map(currPixelTimeMax, timelineStart, timelineEnd, 0, totalTimeInSeconds) - map(currPixelTimeMin, timelineStart, timelineEnd, 0, totalTimeInSeconds);
+    // set increment value based on that value/divisor to keep constant animation speed regardless of time interval selected
+    let animationIncrementValue = curTimeIntervalInSeconds / animationIncrementRateDivisor;
+    if (animationCounter < map(currPixelTimeMax, timelineStart, timelineEnd, 0, totalTimeInSeconds)) animationCounter += animationIncrementValue; // updates animation
     else animation = false;
 }
 
