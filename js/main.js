778916de598b55dd8cc6b9c0b794bb1e03ff484d
BenRydal <ben@benrydal.com>
2021-05-02 13:01:25 -0400

    Update credits

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 /*
-CREDITS/LICENSE INFORMATION: This software is licensed under the GNU General Public License Version 2.0. See the GNU General Public License included with this software for more details. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. IGS software originally developed by Ben Rydal Shapiro at Vanderbilt University as part of his dissertation titled Interaction Geography & the Learning Sciences. Copyright (C) 2018 Ben Rydal Shapiro, and contributers. To reference or read more about this work please see: https://etd.library.vanderbilt.edu/available/etd-03212018-140140/unrestricted/Shapiro_Dissertation.pdf
+CREDITS/LICENSE INFORMATION: 
+This software is written in JavaScript and p5.js and uses YouTube and Kaltura Video Player APIs and the PapaParse library by Matt Holt for CSV file processing.
+This software is licensed under the GNU General Public License Version 2.0. See the GNU General Public License included with this software for more details. 
+IGS-Indoor software was originally developed by Ben Rydal Shapiro at Vanderbilt University
+as part of his dissertation titled Interaction Geography & the Learning Sciences. 
+Copyright (C) 2018 Ben Rydal Shapiro, and contributors. 
+To reference or read more about this work please see: 
+https://etd.library.vanderbilt.edu/available/etd-03212018-140140/unrestricted/Shapiro_Dissertation.pdf
 */
 
 //*************** FILE INPUT AND CORE VARS ***************
