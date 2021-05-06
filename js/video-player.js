a00bfe3e41545be482aa92bf9c79717bec07e097
BenRydal <ben@benrydal.com>
2021-05-06 14:07:00 -0400

    Update videoPlayer interface comments

--- js/video-player.js
+++ js/video-player.js
@@ -1,32 +1,22 @@
 /**********************************************************************************************
-KalturaPlayer, YoutubePlayer and FilePlayer are implementations of a VideoPlayer interface; this
-means that they support the same functionality by having the same set of method names, but behind
-the scenes, achieve the functionality in different ways (by relying on platform-specific JavaScript APIs).
+Additional implementation of VideoPlayer should have the following methods: seekTo(time), play(), pause(), mute(), 
+unMute(), getCurrentTime(), getVideoDuration(), destroy(), show(), hide()
 
-JS is dynamically typed, so we don't actually need to declare a VideoPlayer interface. Any additional 
-implementation of VideoPlayer should have the following methods: seekTo(time), play(), pause(), mute(), 
-unMute(), getCurrentTime(), getVideoDuration(), destroy()
-
-Note the params variable passed into the constructors, this is designed to be a dictionary
-containing any relevant settings that are used to initailize the player to the correct video.
-This params variable is declared in main.js and is called 'videoParams'; note that in setupMovie,
-the 'targetId' is added to the params to specify the div holding the player for Web based players
-like YouTube and Kaltura players. For the File Player, native p5.js methods are used by declaring 
-a movie element in process Video File methods that work with the html5 video plaer.
-
-// ******* FilePlayer Vars *******
+/ ******* FilePlayer Vars *******
 Videoparams expects 1 item, the fileName, videoParams = { fileName: 'your_fileName_here' };
 let videoPlatform = 'File'; // what platform the video is being hosted on, specifies what videoPlayer should be instantiated during setupMovie
 let videoParams = { fileName: '[your_directory_fileLocation]' };
 
 // ******* YouTube Vars *******
-Include following script in head of the format: <script type="text/javascript" src="https://www.youtube.com/iframe_api"></script>
+Include following script in head of the format: 
+<script type="text/javascript" src="https://www.youtube.com/iframe_api"></script>
 VideoParams expects 1 item, the videoId, videoParams = { videoId: 'your_videoId_here' };
 let videoPlatform = 'Youtube'; 
 let videoParams = { videoId: 'Iu0rxb-xkMk'};
 
 // ******* Kaltura Vars *******
-Include following script in head of the format: <script src="https://cdnapi.kaltura.com/p/{partner_id}/sp/{partnerId}00/embedIframeJs/uiconf_id/{uiconf_id}/partner_id/{partnerId}"></script>
+Include following script in head of the format: 
+<script src="https://cdnapi.kaltura.com/p/{partner_id}/sp/{partnerId}00/embedIframeJs/uiconf_id/{uiconf_id}/partner_id/{partnerId}"></script>
 VideoParams expects 3 items, the wid, uiconf_id, and entry_id, videoParams = { wid: 'your_wid_here', uiconf_id: 'your_uiconf_id_here', entry_id: 'your_entry_id_here' };
 var videoPlatform = 'Kaltura';
 var videoParams = { wid: '_1038472', uiconf_id: '33084471', entry_id: '1_9tp4soob' };
