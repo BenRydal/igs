6d45e59ad8228775e4a6127f2797388ed34740ca
BenRydal <ben@benrydal.com>
2021-05-07 13:22:52 -0400

    Update GUI methods and comments

--- js/gui.js
+++ js/gui.js
@@ -1,34 +1,46 @@
+/**
+ * Organizes mousePressed method calls for video, movement/conversation and interaction buttons and path/speaker keys
+ */
 function mousePressed() {
-    // Controls video when clicking on timeline
-    if (videoIsShowing && !animation && overRect(timelineStart, 0, timelineEnd, timelineHeight - tickHeight)) playPauseMovie();
-    textSize(keyTextSize);
-    overMovementConversationKeys();
+    // Controls video when clicking over timeline region
+    if (videoIsShowing && !animation && overRect(timelineStart, 0, timelineEnd, yPosTimelineBottom)) playPauseMovie();
+    overMovementConversationButtons();
+    overInteractionButtons();
     if (showMovementKeys) overPathKeys();
     else overSpeakerKeys();
-    overInteractionButtons();
-    if (!animation && overRect(timelineStart, yPosTimelineTop, timelineLength, timelineThickness)) handleTimeline();
 }
-
+/**
+ * Organizes timeline GUI methods. SelSpacing used to provide additionl "cushion" for mouse.
+ * NOTE: To activate timeline methods, animation mode must be false and 
+ * Either mouse already dragging over timeline OR mouse cursor is over timeline bar.
+ */
 function mouseDragged() {
-    if (!animation) {
-        if (lockedLeft || lockedRight) handleTimeline();
-        else if (overRect(timelineStart, yPosTimelineTop, timelineLength, timelineThickness)) handleTimeline();
-    }
+    if (!animation && ((lockedLeft || lockedRight) || overRect(timelineStart - selSpacing, yPosTimelineTop, timelineLength + selSpacing, timelineThickness))) handleTimeline();
 }
-
+/**
+ * Set locked global vars to false to be able to re-engage timeline GUI methods
+ */
 function mouseReleased() {
     lockedLeft = false;
     lockedRight = false;
 }
-
-function overMovementConversationKeys() {
+/**
+ * Toggles on and off global showMovement var to determine if movement or conversation keys show
+ * NOTE: textSize is way to control dynamic scaling for gui methods and interface
+ */
+function overMovementConversationButtons() {
+    textSize(keyTextSize);
     let currXPos = timelineStart;
     if (overRect(currXPos, speakerKeysHeight, buttonWidth + textWidth("Movement"), buttonWidth)) showMovementKeys = true;
     else if (overRect(currXPos + textWidth("Movement | "), speakerKeysHeight, buttonWidth + textWidth("Conversation"), buttonWidth)) showMovementKeys = false;
 }
 
-// Loop through speakerList and test if over any of speaker keys and update speaker accordingly
+/**
+ * Iterate over global speakerList and test if mouse is over any of speaker keys and update speaker accordingly
+ * NOTE: textSize is way to control dynamic scaling for gui methods and interface
+ */
 function overSpeakerKeys() {
+    textSize(keyTextSize);
     let currXPos = timelineStart + textWidth("Movement | Conversation") + buttonWidth;
     for (let i = 0; i < speakerList.length; i++) {
         let nameWidth = textWidth(speakerList[i].name); // set nameWidth to pixel width of speaker code
@@ -37,17 +49,26 @@ function overSpeakerKeys() {
     }
 }
 
+/**
+ * Iterate over global paths list and test if mouse is over any of paths and update accordingly
+ * NOTE: textSize is way to control dynamic scaling for gui methods and interface
+ */
 function overPathKeys() {
+    textSize(keyTextSize);
     let currXPos = timelineStart + textWidth("Movement | Conversation") + buttonWidth;
     for (let i = 0; i < paths.length; i++) {
-        let nameWidth = textWidth(paths[i].name); // set nameWidth to pixel width of path name
+        const nameWidth = textWidth(paths[i].name); // set nameWidth to pixel width of path name
         if (overRect(currXPos, speakerKeysHeight, buttonWidth + nameWidth, buttonWidth)) paths[i].show = !paths[i].show;
         currXPos += buttonWidth + nameWidth + buttonSpacing;
     }
 }
 
-// Loop through all button/test if mouse clicked and update accordingly
+/**
+ * Iterate over each interaction button and toggle boolean or call corresponding gui method for button
+ * NOTE: textSize is way to control dynamic scaling for gui methods and interface
+ */
 function overInteractionButtons() {
+    textSize(keyTextSize);
     let currXPos = timelineStart + buttonSpacing / 2;
     if (overRect(currXPos, buttonsHeight, textWidth(buttons[0]), buttonWidth)) overAnimateButton();
     else if (overRect(currXPos + textWidth(buttons[0]) + 2 * buttonSpacing, buttonsHeight, textWidth(buttons[1]) + buttonSpacing, buttonWidth)) conversationPositionTop = !conversationPositionTop;
@@ -56,6 +77,9 @@ function overInteractionButtons() {
     else if (overRect(currXPos + textWidth(buttons[0] + buttons[1] + buttons[2] + buttons[3]) + 8 * buttonSpacing, buttonsHeight, textWidth(buttons[4]) + buttonSpacing, buttonWidth)) showIntroMsg = !showIntroMsg;
 }
 
+/**
+ * Toggle on and off animation mode and set/end global animation counter variable
+ */
 function overAnimateButton() {
     if (animation) {
         animation = false;
@@ -66,11 +90,13 @@ function overAnimateButton() {
     }
 }
 
+/**
+ * Updates currPixelTimeMin or currPixelTimeMax global variables depending on left or right selector that user is over
+ * NOTE: Is triggered if user already dragging or begins dragging
+ */
 function handleTimeline() {
-    let xPosLeftSelector = currPixelTimeMin;
-    let xPosRightSelector = currPixelTimeMax;
-
-    // 3 types of selection that work together
+    const xPosLeftSelector = currPixelTimeMin;
+    const xPosRightSelector = currPixelTimeMax;
     if (lockedLeft || (!lockedRight && overRect(xPosLeftSelector - selSpacing, yPosTimelineTop, 2 * selSpacing, timelineThickness))) {
         lockedLeft = true;
         currPixelTimeMin = constrain(mouseX, timelineStart, timelineEnd);
@@ -82,7 +108,10 @@ function handleTimeline() {
     }
 }
 
-// VIDEO
+/**
+ * Toggle whether video is playing and whether video is showing
+ * NOTE: this is different than playPauseMovie method
+ */
 function overVideoButton() {
     if (videoIsShowing) {
         videoPlayer.pause();
@@ -91,28 +120,31 @@ function overVideoButton() {
     } else {
         videoPlayer.show();
     }
-    videoIsShowing = !videoIsShowing;
+    videoIsShowing = !videoIsShowing; // set after testing
 }
 
-// Plays/pauses video and toggles videoIsPlaying
+/**
+ * Plays/pauses movie and updates videoPlayhead if setting to play
+ * Also toggles global videoIsPlaying variable
+ */
 function playPauseMovie() {
     if (videoIsPlaying) {
         videoPlayer.pause();
         videoIsPlaying = false;
     } else {
-        let mPos = map(mouseX, timelineStart, timelineEnd, currPixelTimeMin, currPixelTimeMax); // first map mouse to selected time values in GUI
+        // first map mouse to selected time values in GUI
+        const mapMousePos = map(mouseX, timelineStart, timelineEnd, currPixelTimeMin, currPixelTimeMax);
         // must floor vPos to prevent double finite error
-        let vPos = Math.floor(map(mPos, timelineStart, timelineEnd, 0, Math.floor(videoPlayer.getVideoDuration())));
+        const videoPos = Math.floor(map(mapMousePos, timelineStart, timelineEnd, 0, Math.floor(videoPlayer.getVideoDuration())));
         videoPlayer.play();
-        videoPlayer.seekTo(vPos);
+        videoPlayer.seekTo(videoPos);
         videoIsPlaying = true;
     }
 }
 
-// Tests if over circle with x, y and diameter
 function overCircle(x, y, diameter) {
-    let disX = x - mouseX;
-    let disY = y - mouseY;
+    const disX = x - mouseX;
+    const disY = y - mouseY;
     return sqrt(sq(disX) + sq(disY)) < diameter / 2;
 }
 
