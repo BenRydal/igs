67cdb3ac0877c81c90190819cc840f63b626a324
BenRydal <ben@benrydal.com>
2021-09-30 16:04:18 -0400

    Update comments/move styles

--- js/main.js
+++ js/main.js
@@ -20,12 +20,7 @@ const igs = new p5((sk) => {
     }
 
     sk.setup = function () {
-        // CANVAS/STYLES
         sk.canvas = sk.createCanvas(window.innerWidth, window.innerHeight, sk.WEBGL);
-        sk.textSize(sk.GUITEXTSIZE);
-        sk.textFont(sk.font_Lato);
-        sk.textAlign(sk.LEFT, sk.TOP);
-        sk.smooth();
         // SINGLETONS
         sk.core = new Core(sk); // holds core data, update and parsing methods/classes
         sk.gui = new GUI(sk); // holds GUI elements/classes
@@ -38,6 +33,11 @@ const igs = new p5((sk) => {
         sk.DRAWGUI = 0;
         sk.HANDLEGUI = 1;
         sk.GUITEXTSIZE = sk.width / 70;
+        // STYLES
+        sk.textSize(sk.GUITEXTSIZE);
+        sk.textFont(sk.font_Lato);
+        sk.textAlign(sk.LEFT, sk.TOP);
+        sk.smooth();
     }
 
     sk.draw = function () {
