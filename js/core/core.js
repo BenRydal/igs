6429745fd6a8cf6918c90429bef9c6c241671178
BenRydal <ben@benrydal.com>
2021-11-09 14:48:40 -0500

    Update code to start on true and checked colors

--- js/core/core.js
+++ js/core/core.js
@@ -96,7 +96,7 @@ class Core {
     createCode(name) {
         return {
             name, // Char first letter of filename
-            isShowing: false, // if displaying in GUI
+            isShowing: true, // if displaying in GUI
             color: this.createColorForGUI(this.sk.COLORGRAY, this.COLOR_LIST[this.codeList.length % this.COLOR_LIST.length])
         };
     }
