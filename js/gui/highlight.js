ff0d4fabc68adf143a89f1bc0c0b1d38c10740ff
BenRydal <ben@benrydal.com>
2022-02-22 17:05:09 -0500

    Update comments

--- js/gui/highlight.js
+++ js/gui/highlight.js
@@ -59,10 +59,18 @@ class Highlight {
         for (const highlightRect of this.highlightArray) this.drawHighlightRects(highlightRect);
     }
 
+    /**
+     * Current highlight being recorded is drawn simply as a rectangle based on user mouse position and a starting point
+     */
     drawCurHighlight() {
         this.sk.rect(this.curXTop, this.curYTop, this.sk.mouseX - this.curXTop, this.sk.mouseY - this.curYTop);
     }
 
+    /**
+     * Organizes drawing of rects in 2D or 3D
+     * NOTE: Simple rectangle can be used to draw all highlightRects in 2D and also in 3D if it was not highlighted in space-time/over the timeline
+     * @param  {HighlightRect} highlightRect
+     */
     drawHighlightRects(highlightRect) {
         if (this.sk.handle3D.getIs3DMode() && this.sk.gui.timelinePanel.overAxis(highlightRect.xPos)) {
             this.draw3DHighlightRect(highlightRect);
@@ -71,22 +79,30 @@ class Highlight {
         }
     }
 
+    /**
+     * Draws 3D cube corresponding to time selected by user (vertical axis)
+     * NOTE: rect is drawn across entire floorPlan along the x dimension/axis while the y is constrained to user selection (in other words, a user can't select portions of the x axis when user highlights over the timeline)
+     */
     draw3DHighlightRect(highlightRect) {
         // Map method maintains highlight rect scaling if user adjusts timeline
         const zPosStart = this.sk.sketchController.mapToSelectTimeThenPixelTime(highlightRect.xPos);
         const zPosEnd = this.sk.sketchController.mapToSelectTimeThenPixelTime(highlightRect.xPos + highlightRect.width);
         const span = this.sk.gui.fpContainer.getContainer().width; // span 3D rects full span of floor plan container
-        // top
+        // cube top
         this.sk.quad(0, highlightRect.yPos, zPosEnd, span, highlightRect.yPos, zPosEnd, span, highlightRect.yPos + highlightRect.height, zPosEnd, 0, highlightRect.yPos + highlightRect.height, zPosEnd);
-        // bottom
+        // cube bottom
         this.sk.quad(0, highlightRect.yPos, zPosStart, span, highlightRect.yPos, zPosStart, span, highlightRect.yPos + highlightRect.height, zPosStart, 0, highlightRect.yPos + highlightRect.height, zPosStart);
-        // line connectors
+        // connectors
         this.sk.line(0, highlightRect.yPos, zPosEnd, 0, highlightRect.yPos, zPosStart);
         this.sk.line(span, highlightRect.yPos, zPosEnd, span, highlightRect.yPos, zPosStart);
         this.sk.line(0, highlightRect.yPos + highlightRect.height, zPosEnd, 0, highlightRect.yPos + highlightRect.height, zPosStart);
         this.sk.line(span, highlightRect.yPos + highlightRect.height, zPosEnd, span, highlightRect.yPos + highlightRect.height, zPosStart);
     }
 
+    /**
+     * Called from draw movement/conversation methods (params are numbers from CurPos object)
+     * Returns true if no highlightRects or if data is within bounds of a highlighRect
+     */
     overHighlightArray(xPos, yPos, xPosTime) {
         if (!this.highlightArray.length) return true; // return true if no data
         else {
@@ -97,6 +113,9 @@ class Highlight {
         }
     }
 
+    /**
+     * NOTE: If in 3D mode, you need to first test if highlightRect was selected on floor plan or timeline
+     */
     overHighlightRect(highlightRect, xPos, yPos, xPosTimeToMap) {
         const xPosTime = this.sk.sketchController.mapSelectTimeToPixelTime2D(xPosTimeToMap); // Map method maintains highlight rect scaling if user adjusts timeline, make sure to test 2D only
         if (this.sk.handle3D.getIs3DMode()) {
