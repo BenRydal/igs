685c9a54e4ba3c9875026a7be17e0058ea329ef4
BenRydal <ben@benrydal.com>
2022-05-17 15:35:22 -0400

    Create separate addListeners moduel

--- js/main.js
+++ js/main.js
@@ -20,6 +20,9 @@ import {
     DomController
 } from './dom-gui/dom-controller.js';
 import {
+    addListeners
+} from './dom-gui/add-listeners.js';
+import {
     SketchController
 } from './sketch/sketch-controller.js';
 import {
@@ -35,6 +38,7 @@ import {
 import {
     DrawMovement
 } from './draw/draw-movement.js';
+
 
 
 const igs = new p5((sk) => {
@@ -63,73 +67,7 @@ const igs = new p5((sk) => {
         sk.textFont(sk.font_Lato);
         sk.textAlign(sk.LEFT, sk.TOP);
         sk.smooth(); // must enable when using WEBGL renderer
-        sk.addListeners();
-    }
-
-    sk.addListeners = function () {
-        // Main Menu Bar
-        document.getElementById("data-drop-down-menu").addEventListener("change", sk.domHandler.handleExampleDropDown.bind(sk.domHandler));
-        document.getElementById("input-load-files").addEventListener("change", sk.domHandler.handleLoadFileButton.bind(sk.domHandler));
-        document.getElementById("clear-button").addEventListener("click", sk.domHandler.handleClearButton.bind(sk.domHandler));
-        document.getElementById("3D-button").addEventListener("click", sk.domHandler.handleToggle3DButton.bind(sk.domHandler));
-        document.getElementById("how-to-button").addEventListener("click", sk.domHandler.handleHowToButton.bind(sk.domHandler));
-        // Tab 1
-        document.getElementById("sub-tab1-1").addEventListener("change", function () {
-            sk.domController.updateSubTab(this.checked, "movement");
-        });
-        // Tab 2 Talk
-        document.getElementById("sub-tab3-1").addEventListener("change", sk.domHandler.handleWordSearchInput.bind(sk.domHandler));
-        document.getElementById("sub-tab3-2").addEventListener("click", sk.sketchController.toggleIsAlignTalk.bind(sk.sketchController));
-        document.getElementById("sub-tab3-3").addEventListener("click", sk.sketchController.toggleIsAllTalk.bind(sk.sketchController));
-        document.getElementById("sub-tab3-4").addEventListener("change", function () {
-            sk.domController.updateSubTab(this.checked, "talk");
-        });
-        // Tab 3 Video
-        document.getElementById("sub-tab4-1").addEventListener("click", sk.sketchController.toggleShowVideo.bind(sk.sketchController));
-        document.getElementById("sub-tab4-2").addEventListener("click", sk.sketchController.playPauseVideoFromButton.bind(sk.sketchController));
-        document.getElementById("sub-tab4-3").addEventListener("click", sk.sketchController.increaseVideoSize.bind(sk.sketchController));
-        document.getElementById("sub-tab4-4").addEventListener("click", sk.sketchController.decreaseVideoSize.bind(sk.sketchController));
-        // Tab 4 Animate 
-        document.getElementById("sub-tab5-1").addEventListener("click", sk.sketchController.startEndAnimation.bind(sk.sketchController));
-        document.getElementById("sub-tab5-2").addEventListener("click", sk.sketchController.toggleIsAnimatePause.bind(sk.sketchController));
-        // Tab 5 Select
-        document.getElementById("sub-tab6-1").addEventListener("click", function () {
-            sk.sketchController.setCurSelectTab(0);
-            sk.gui.highlight.resetHighlightArray();
-        });
-        document.getElementById("sub-tab6-2").addEventListener("click", function () {
-            sk.sketchController.setCurSelectTab(1);
-        });
-        document.getElementById("sub-tab6-3").addEventListener("click", function () {
-            sk.sketchController.setCurSelectTab(2);
-        });
-        document.getElementById("sub-tab6-4").addEventListener("click", function () {
-            sk.sketchController.setCurSelectTab(3);
-        });
-        document.getElementById("sub-tab6-5").addEventListener("click", function () {
-            sk.sketchController.setCurSelectTab(4);
-        });
-        document.getElementById("sub-tab6-6").addEventListener("click", function () {
-            sk.sketchController.setCurSelectTab(5);
-        });
-
-        // Tab 6 Floor Plan
-        document.getElementById("sub-tab7-1").addEventListener("click", sk.floorPlan.setRotateLeft.bind(sk.floorPlan));
-        document.getElementById("sub-tab7-2").addEventListener("click", sk.floorPlan.setRotateRight.bind(sk.floorPlan));
-
-        // Tab 7 Codes
-        document.getElementById("sub-tab8-1").addEventListener("click", sk.domHandler.handleColorModeButton.bind(sk.domHandler));
-        document.getElementById("sub-tab8-2").addEventListener("change", function () {
-            sk.domController.updateSubTab(this.checked, "codes");
-        });
-
-        // Update sketch loop for specific tabs/elements
-        document.querySelectorAll('.loop-sketch').forEach(element => {
-            element.addEventListener('click', function () {
-                sk.loop();
-            });
-        });
-
+        addListeners(sk);
     }
 
     sk.draw = function () {
