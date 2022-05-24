9f4391506c06f22f42531daa0eef3563f98d977e
BenRydal <ben@benrydal.com>
2022-05-24 14:18:46 -0400

    Add blank create code file

---
+++ js/draw/create-code-file.js
@@ -0,0 +1,72 @@
+// TODO:
+// move pathList test in main???
+// Remove codeList from vars in draw movement / convo
+// move methods to drawUtils
+// add createcodefile class
+// add html button and listeners
+
+// new CreateCodeFile(this.sk, pathList, codeList);
+
+import { DrawMovement } from './draw-movement.js';
+import { DrawUtils } from './draw-utils.js';
+
+export class CreateCodeFile {
+
+    constructor(sketch, pathList, codeList) {
+        this.sk = sketch;
+        this.pathList = pathList;
+        this.drawUtils = new DrawUtils(sketch, codeList);
+    }
+
+    createCodeFile() {
+        if (this.sk.arrayIsLoaded(this.pathList)) {
+            const drawMovement = new DrawMovement(this.sk, this.drawUtils);
+            for (const path of this.pathList) {
+                if (path.isShowing) {
+                    const [startTimesArray, endTimesArray] = drawMovement.getCodeFileArrays(path);
+                    // TODO need to save and write the file for EVERY path--save it with path name?
+                    // test if length of startTimes === path length
+                    this.sk.saveTable(this.writeTable(startTimesArray, endTimesArray), path.name, "csv");
+                }
+            }
+        }
+    }
+
+    // TODO: don't pass view
+    // * 
+    getCodeFileArrays(view, movementArray) {
+        let startTimesArray = [];
+        let endTimesArray = [];
+        let zzzRecordingCode = false;
+        for (let i = 1; i < movementArray.length; i++) { // start at 1 to allow testing of current and prior indices
+            const p = this.createComparePoint(view, movementArray[i], movementArray[i - 1]); // a compare point consists of current and prior augmented points
+            if (this.isVisible(p.cur)) {
+                if (zzzRecordingCode === false) {
+                    zzzRecordingCode = true;
+                    startTimesArray.push(p.cur.point.time);
+                }
+            } else {
+                if (zzzRecordingCode === true) {
+                    zzzRecordingCode = false;
+                    endTimesArray.push(p.prior.point.time);
+                }
+            }
+        }
+        // for last point in case still recording?
+        // if (zzzRecordingCode === true) endTimesArray.push(p.prior.point.time);
+        return [startTimesArray, endTimesArray];
+    }
+
+    writeTable(startTimesArray, endTimesArray) {
+        const headers = ["start", "end"];
+        let table = new p5.Table();
+        table.addColumn(headers[0]);
+        table.addColumn(headers[1]);
+        for (let i = 0; i < startTimesArray.length; i++) {
+            let newRow = table.addRow();
+            newRow.setNum(headers[0], startTimesArray[i]);
+            newRow.setNum(headers[1], endTimesArray[i]);
+        }
+        return table;
+    }
+}
