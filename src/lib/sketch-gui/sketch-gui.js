00d2a66305ccb4da597d43acd753ad987735a144
BenRydal <ben@benrydal.com>
2024-01-31 11:52:57 -0500

    Remove old timelinePanel.js, move slicer drawing methods to sketch-gui

--- src/lib/sketch-gui/sketch-gui.js
+++ src/lib/sketch-gui/sketch-gui.js
@@ -1,4 +1,3 @@
-import { TimelinePanel } from './timeline-panel.js';
 import { FloorPlanContainer } from './floorplan-container.js';
 import { Highlight } from './highlight.js';
 
@@ -6,13 +5,12 @@ export class SketchGUI {
 	constructor(sketch) {
 		this.sk = sketch;
 		this.displayBottom = this.sk.height;
-		this.timelinePanel = new TimelinePanel(this.sk);
 		this.fpContainer = new FloorPlanContainer(this.sk, this.sk.sketchController.getTimelineStartXPos(), this.displayBottom);
 		this.highlight = new Highlight(this.sk, this.displayBottom);
 	}
 
 	update2D() {
-		if (this.sk.sketchController.overTimeline() && !this.sk.handle3D.getIs3DMode()) this.timelinePanel.drawLongSlicer();
+		if (this.sk.sketchController.overTimeline() && !this.sk.handle3D.getIs3DMode()) this.drawLongSlicer();
 
 		if (!this.sk.handle3D.getIs3DModeOrTransitioning()) {
 			if (this.sk.sketchController.getCurSelectTab() === 1) this.fpContainer.drawRegionSelector();
@@ -23,7 +21,25 @@ export class SketchGUI {
 	update3D() {
 		this.highlight.setDraw();
 		if (this.sk.handle3D.getIs3DMode() && this.sk.sketchController.overTimeline()) {
-			this.timelinePanel.draw3DSlicerRect(this.fpContainer.getContainer(), this.sk.sketchController.mapToSelectTimeThenPixelTime(this.sk.mouseX));
+			this.draw3DSlicerRect(this.fpContainer.getContainer(), this.sk.sketchController.mapToSelectTimeThenPixelTime(this.sk.mouseX));
 		}
+	}
+
+	setSlicerStroke() {
+		this.sk.fill(0);
+		this.sk.stroke(0);
+		this.sk.strokeWeight(2);
+	}
+
+	drawLongSlicer() {
+		this.setSlicerStroke();
+		this.sk.line(this.sk.mouseX, 0, this.sk.mouseX, this.height);
+	}
+
+	draw3DSlicerRect(container, zPos) {
+		this.sk.fill(255, 50);
+		this.sk.stroke(0);
+		this.sk.strokeWeight(1);
+		this.sk.quad(0, 0, zPos, container.width, 0, zPos, container.width, container.height, zPos, 0, container.height, zPos);
 	}
 }
