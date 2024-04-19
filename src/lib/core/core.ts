47786cd6f5d7d63b995d741004f5666be70d5f86
BenRydal <ben@benrydal.com>
2024-04-19 09:49:50 -0400

    Moved userColors to constants, removed unused theme file

--- src/lib/core/core.ts
+++ src/lib/core/core.ts
@@ -1,19 +1,3 @@
-const USER_COLORS = [
-	'#6a3d9a', // Dark Purple
-	'#ff7f00', // Dark Orange
-	'#33a02c', // Dark Green
-	'#1f78b4', // Dark Blue
-	'#e31a1c', // Dark Red
-	'#b15928', // Dark Brown
-	'#cab2d6', // Light Purple
-	'#fdbf6f', // Light Orange
-	'#b2df8a', //Light Green
-	'#a6cee3', //Light Blue
-	'#fb9a99', //Light Red
-	'#ffed6f' //Yellow
-	// Add more colors as needed
-];
-
 /**
  * This class holds core program data and associated parsing methods from processed CSV files.
  * Separate parsing classes for movement, conversation, and code CSV files test parsed data from PapaParse
@@ -31,7 +15,7 @@ import { ParseCodes } from './parse-codes.js';
 
 import { DataPoint } from '../../models/dataPoint.js';
 import { User } from '../../models/user.js';
-import * as Constants from '../constants/index.js';
+import { USER_COLORS } from '../constants/index.js';
 
 import UserStore from '../../stores/userStore';
 import TimelineStore from '../../stores/timelineStore';
