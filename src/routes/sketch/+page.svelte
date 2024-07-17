39350c2cff97c6217b28a7a53bde4b6ce8b0c2de
Edwin Zhao <contact@edwinzhao.com>
2024-07-17 01:14:30 -0700

    feat: working version of nav bar item

--- src/routes/sketch/+page.svelte
+++ src/routes/sketch/+page.svelte
@@ -99,195 +99,168 @@
 	}
 </script>
 
-<div class="drawer">
-	<input id="my-drawer" type="checkbox" class="drawer-toggle" />
-	<div class="navbar min-h-16 bg-[#f6f5f3] text-base-100">
-		<div class="flex-1 px-2 lg:flex-none">
-			<a class="text-lg font-bold" href="/">IGS</a>
-			<button class="btn btn-sm ml-4" on:click={() => (showDataPopup = true)}>Show Data</button>
-		</div>
-
-		<div class="flex justify-end flex-1 px-2">
-			<div class="flex items-stretch">
-				<IconButton
-					id="btn-rotate-left"
-					icon={MdRotateLeft}
-					tooltip={'Rotate Left'}
-					on:click={() => {
-						p5Instance.floorPlan.setRotateLeft();
-						p5Instance.loop();
-					}}
-				/>
-
-				<IconButton
-					id="btn-rotate-left"
-					icon={MdRotateRight}
-					tooltip={'Rotate Right'}
-					on:click={() => {
-						p5Instance.floorPlan.setRotateRight();
-						p5Instance.loop();
-					}}
-				/>
-
-				<IconButton icon={MdCloudDownload} tooltip={'Download your Data'} />
-				<div
-					data-tip="Upload"
-					class="tooltip tooltip-bottom btn capitalize icon max-h-8 bg-[#f6f5f3] border-[#f6f5f3]"
-					role="button"
-					tabindex="0"
-					on:click
-					on:keydown
-				>
-					<label for="file-input">
-						<MdCloudUpload />
-					</label>
-				</div>
-
-				<input
-					class="hidden"
-					id="file-input"
-					multiple
-					accept=".png, .jpg, .jpeg, .csv, .mp4"
-					type="file"
-					bind:files
-					on:change={core.handleUserLoadedFiles}
-				/>
-
-				<IconButton icon={MdHelpOutline} tooltip={'Help'} on:click={() => ($isModalOpen = !$isModalOpen)} />
-
-				<IconButton
-					id="btn-toggle-3d"
-					icon={Md3DRotation}
-					tooltip={'Toggle 3D'}
-					on:click={() => {
-						p5Instance.handle3D.update();
-					}}
-				/>
-
-				{#if isVideoShowing}
-					<IconButton id="btn-toggle-video" icon={MdVideocam} tooltip={'Show/Hide Video'} on:click={toggleVideo} />
-				{:else}
-					<IconButton id="btn-toggle-video" icon={MdVideocamOff} tooltip={'Show/Hide Video'} on:click={toggleVideo} />
-				{/if}
-				<select
-					id="select-data-dropdown"
-					class="select select-bordered w-full max-w-xs bg-[#f6f5f3] text-black"
-					on:change={core.handleExampleDropdown}
-				>
-					<option disabled selected>-- Select an Example --</option>
-					<option value="example-1">Michael Jordan's Last Shot</option>
-					<option value="example-2">Family Museum Gallery Visit</option>
-					<option value="example-3">Classroom Science Lesson</option>
-					<option value="example-4">Classroom Discussion</option>
-				</select>
-			</div>
-		</div>
+<div class="navbar min-h-16 bg-[#f6f5f3] text-base-100">
+	<div class="flex-1 px-2 lg:flex-none">
+		<a class="text-lg font-bold" href="/">IGS</a>
+		<button class="btn btn-sm ml-4" on:click={() => (showDataPopup = true)}>Show Data</button>
 	</div>
 
-	<P5 {sketch} />
+	<div class="flex justify-end flex-1 px-2">
+		<div class="flex items-stretch">
+			<IconButton
+				id="btn-rotate-left"
+				icon={MdRotateLeft}
+				tooltip={'Rotate Left'}
+				on:click={() => {
+					p5Instance.floorPlan.setRotateLeft();
+					p5Instance.loop();
+				}}
+			/>
 
-	{#if showDataPopup}
-		<div
-			class="modal modal-open"
-			on:click|self={() => (showDataPopup = false)}
-			on:keydown={(e) => {
-				if (e.key === 'Escape') showDataPopup = false;
-			}}
-		>
-			<div class="modal-box relative">
-				<button class="btn btn-sm btn-circle absolute right-2 top-2" on:click={() => (showDataPopup = false)}>
-					<span class="material-symbols-outlined">close</span>
-				</button>
-				<h3 class="font-bold text-lg">Data</h3>
-				<div class="overflow-x-auto">
-					<h4 class="font-bold">Codes:</h4>
-					<ul>
-						{#each $CodeStore as code}
-							<li>{code.code}</li>
-						{/each}
-					</ul>
+			<IconButton
+				id="btn-rotate-left"
+				icon={MdRotateRight}
+				tooltip={'Rotate Right'}
+				on:click={() => {
+					p5Instance.floorPlan.setRotateRight();
+					p5Instance.loop();
+				}}
+			/>
 
-					<h4 class="font-bold mt-4">Users:</h4>
-					{#each $UserStore as user}
-						<div class="mb-4">
-							<button class="btn btn-sm" on:click={() => toggleUserExpansion(user.name)}>
-								{user.name}
-							</button>
-							{#if expandedUsers[user.name]}
-								<div class="ml-4">
-									<p>Color: {user.color}</p>
-									<p>Enabled: {user.enabled}</p>
-									<h5 class="font-bold">Data Points:</h5>
-									<DataPointTable dataPoints={user.dataTrail} />
-								</div>
-							{/if}
-						</div>
-					{/each}
-				</div>
-				<div class="modal-action">
-					<button class="btn" on:click={() => (showDataPopup = false)}>Close</button>
-				</div>
+			<IconButton icon={MdCloudDownload} tooltip={'Download your Data'} />
+			<div
+				data-tip="Upload"
+				class="tooltip tooltip-bottom btn capitalize icon max-h-8 bg-[#f6f5f3] border-[#f6f5f3]"
+				role="button"
+				tabindex="0"
+				on:click
+				on:keydown
+			>
+				<label for="file-input">
+					<MdCloudUpload />
+				</label>
 			</div>
+
+			<input
+				class="hidden"
+				id="file-input"
+				multiple
+				accept=".png, .jpg, .jpeg, .csv, .mp4"
+				type="file"
+				bind:files
+				on:change={core.handleUserLoadedFiles}
+			/>
+
+			<IconButton icon={MdHelpOutline} tooltip={'Help'} on:click={() => ($isModalOpen = !$isModalOpen)} />
+
+			<IconButton
+				id="btn-toggle-3d"
+				icon={Md3DRotation}
+				tooltip={'Toggle 3D'}
+				on:click={() => {
+					p5Instance.handle3D.update();
+				}}
+			/>
+
+			{#if isVideoShowing}
+				<IconButton id="btn-toggle-video" icon={MdVideocam} tooltip={'Show/Hide Video'} on:click={toggleVideo} />
+			{:else}
+				<IconButton id="btn-toggle-video" icon={MdVideocamOff} tooltip={'Show/Hide Video'} on:click={toggleVideo} />
+			{/if}
+			<select id="select-data-dropdown" class="select select-bordered w-full max-w-xs bg-[#f6f5f3] text-black" on:change={core.handleExampleDropdown}>
+				<option disabled selected>-- Select an Example --</option>
+				<option value="example-1">Michael Jordan's Last Shot</option>
+				<option value="example-2">Family Museum Gallery Visit</option>
+				<option value="example-3">Classroom Science Lesson</option>
+				<option value="example-4">Classroom Discussion</option>
+			</select>
 		</div>
-	{/if}
+	</div>
+</div>
 
-	<!-- TODO: @Ben see resources here:
-   https://daisyui.com/components/dropdown/#dropdown-top--aligns-to-end
-   -->
-	<div class="btm-nav flex justify-between">
-		<!-- Left Side: Select and Carousel -->
-		<!-- <div class="w-1/2 overflow-x-auto bg-[#f6f5f3]">
-			<div class="carousel carousel-center max-w-full p-4 space-x-4">
-				{#each users as user, index}
-					<details class="dropdown dropdown-top carousel-item z-100">
-						<summary class="m-1 btn">{user.name}</summary>
-						<ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-							<li>
-								<button class="btn" on:click={() => core.handleUserVisibility(user.name)}>
-									{user.enabled ? 'Hide' : 'Show'}
-								</button>
-							</li>
-							<li>
-								<button class="btn" on:click={() => core.handleUserColor(user.name)}> Change Color </button>
-							</li>
-							<li>
-								<button class="btn" on:click={() => core.handleUserSpeech(user.name)}>
-									{showSpeechPopup[user.name] ? 'Hide' : 'Show'} Speech
-								</button>
-							</li>
-							<li>
-								<button class="btn" on:click={() => core.handleUserCode(user.name)}>
-									{showCodePopup[user.name] ? 'Hide' : 'Show'} Code
-								</button>
-							</li>
-						</ul>
-					</details>
-				{/each}
-			</div>
-		</div> -->
+<div class="h-10">
+	<P5 {sketch} />
+</div>
 
-		<div class="w-1/2 overflow-x-auto bg-[#f6f5f3]">
-			<div class="flex space-x-4">
-				{#each users as user, index}
-					<div class="dropdown dropdown-top dropdown-end">
-						<div tabindex={index} role="button" class="btn">{user.name}</div>
-						<ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-							<li><a>Item 1</a></li>
-							<li><a>Item 2</a></li>
-						</ul>
+{#if showDataPopup}
+	<div
+		class="modal modal-open"
+		on:click|self={() => (showDataPopup = false)}
+		on:keydown={(e) => {
+			if (e.key === 'Escape') showDataPopup = false;
+		}}
+	>
+		<div class="modal-box relative">
+			<button class="btn btn-sm btn-circle absolute right-2 top-2" on:click={() => (showDataPopup = false)}>
+				<span class="material-symbols-outlined">close</span>
+			</button>
+			<h3 class="font-bold text-lg">Data</h3>
+			<div class="overflow-x-auto">
+				<h4 class="font-bold">Codes:</h4>
+				<ul>
+					{#each $CodeStore as code}
+						<li>{code.code}</li>
+					{/each}
+				</ul>
+
+				<h4 class="font-bold mt-4">Users:</h4>
+				{#each $UserStore as user}
+					<div class="mb-4">
+						<button class="btn btn-sm" on:click={() => toggleUserExpansion(user.name)}>
+							{user.name}
+						</button>
+						{#if expandedUsers[user.name]}
+							<div class="ml-4">
+								<p>Color: {user.color}</p>
+								<p>Enabled: {user.enabled}</p>
+								<h5 class="font-bold">Data Points:</h5>
+								<DataPointTable dataPoints={user.dataTrail} />
+							</div>
+						{/if}
 					</div>
 				{/each}
 			</div>
+			<div class="modal-action">
+				<button class="btn" on:click={() => (showDataPopup = false)}>Close</button>
+			</div>
 		</div>
+	</div>
+{/if}
 
-		<!-- Right Side: Timeline -->
-		<div class="w-1/2 overflow-x-auto bg-[#f6f5f3]">
-			<TimelinePanel />
+<div class="btm-nav flex justify-between">
+	<div class="w-1/2 overflow-x-overflow bg-[#f6f5f3]">
+		<div class="flex space-x-4">
+			{#each $UserStore as user}
+				<div class="dropdown dropdown-top dropdown-end">
+					<div tabindex={0} role="button" class="btn">{user.name}</div>
+					<ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+						<li>
+							<div class="flex items-center">
+								<input id="userCheckbox-{user.name}" type="checkbox" class="checkbox" bind:checked={user.enabled} />
+								Enabled
+							</div>
+						</li>
+						<li>
+							<div class="flex items-center">
+								<label for="userCheckbox-{user.name}" class="flex items-center ml-1 mr-4">
+									<input type="color" class="color-picker" bind:value={user.color} />
+									Color
+								</label>
+							</div>
+						</li>
+					</ul>
+				</div>
+			{/each}
 		</div>
 	</div>
 
-	<slot />
+	<!-- Right Side: Timeline -->
+	<div class="w-1/2 overflow-x-auto bg-[#f6f5f3]">
+		<TimelinePanel />
+	</div>
 </div>
+
+<slot />
 
 <IgsInfoModal {isModalOpen} />
 
@@ -295,11 +268,6 @@
 	.navbar {
 		height: 64px;
 	}
-
-	.btm-nav {
-		height: 100px;
-	}
-
 	.color-picker {
 		width: 30px;
 		height: 30px;
