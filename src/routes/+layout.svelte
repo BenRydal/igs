0b910123da1a349de95d771b9d233057a1c8185d
ez100001 <contact@edwinzhao.com>
2024-10-02 00:32:51 -0700

    ci: testing empty commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,5 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
-
-<!-- <div class="navbar min-h-16 bg-neutral text-base-100">
-  <div class="flex-1 px-2 lg:flex-none">
-    <a class="text-lg font-bold" href="/">IGS</a>
-  </div>
-  <div class="flex justify-end flex-1 px-2">
-    <div class="flex items-stretch">
-      <a class="btn btn-ghost rounded-btn" href="https://github.com/BenRydal">More like this</a>
-      <a class="btn btn-ghost rounded-btn" href="/help">Help</a>
-      <div class="dropdown dropdown-end dropdown-hover">
-        <label tabindex="-1" class="btn btn-ghost rounded-btn" for="ul">Examples</label>
-        <ul tabindex="-1" class="menu dropdown-content p-2 shadow bg-neutral rounded-box w-52">
-          <li><a href='/examples/jordan'>Michael Jordan's Last Shot</a></li>
-          <li><a href='/examples/lesson'>Classroom Science Lesson</a></li>
-          <li><a href='/examples/discussion'>Classroom Discussion</a></li>
-          <li><a href='/examples/gallery'>Family's Gallery Visit</a></li>
-        </ul>
-      </div>
-      <a class="btn btn-ghost rounded-btn" href="/settings">Settings</a>
-    </div>
-  </div>
-</div> -->
 
 <slot />
