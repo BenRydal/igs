7a80c31775b2790cd7332301fb588d67b7d241ef
Edwin Zhao <contact@edwinzhao.com>
2024-10-17 08:14:37 -0700

    feat: changed sketch path to slicer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,8 @@
 	<div class="hero-content text-center text-neutral-content">
 		<div class="max-w-md text-white">
 			<h1 class="mb-5 text-5xl font-bold text-default">Interaction Geography Slicer</h1>
-			<p class="mb-5 text-xl">
-				An open-source tool to dynamically visualize movement, conversation, and
-				video data over space and time.
-			</p>
-			<a role="button" class="btn" href="/sketch">Start Slicing</a>
+			<p class="mb-5 text-xl">An open-source tool to dynamically visualize movement, conversation, and video data over space and time.</p>
+			<a role="button" class="btn" href="/slicer">Start Slicing</a>
 		</div>
 	</div>
 </div>
